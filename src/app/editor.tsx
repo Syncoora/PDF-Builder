3d783f5f94ea28f9ea4ae1c85875f2acf00d9b66
GioCharkviani <giorgi.charkviani00@gmail.com>
2025-03-30 11:06:09 +0400

    remove node files

--- src/app/editor.tsx
+++ src/app/editor.tsx
@@ -96,20 +96,22 @@ const FontSize = Extension.create({
     ];
   },
 
-  // addCommands() {
-  //   return {
-  //     setFontSize:
-  //       (fontSize) =>
-  //       ({ chain }) => {
-  //         return chain().setMark("textStyle", { fontSize }).run();
-  //       },
-  //     unsetFontSize:
-  //       () =>
-  //       ({ chain }) => {
-  //         return chain().setMark("textStyle", { fontSize: null }).run();
-  //       },
-  //   };
-  // },
+  /* Commenting out addCommands to fix build error
+  addCommands() {
+    return {
+      setFontSize:
+        (fontSize) =>
+        ({ chain }) => {
+          return chain().setMark("textStyle", { fontSize }).run()
+        },
+      unsetFontSize:
+        () =>
+        ({ chain }) => {
+          return chain().setMark("textStyle", { fontSize: null }).run()
+        },
+    }
+  },
+  */
 });
 
 // Add this CSS at the top of your editor styles
@@ -396,7 +398,8 @@ const Editor = forwardRef<EditorRef, EditorProps>(
 
     // Add this helper function for font size commands
     const setFontSize = (size: string) => {
-      editor?.chain().focus().setFontSize(size).run();
+      // Use a more direct approach instead of the custom command
+      editor?.chain().focus().setMark("textStyle", { fontSize: size }).run();
     };
 
     const insertTable = () => {
